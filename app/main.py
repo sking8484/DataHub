20e8f602710ed630988da1f1862eb3e8bb2f8854
sking8484 <sking8484@gmail.com>
2022-09-04 16:07:11 -0400

    Allowing any origin and CORS

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ from utils.db.database import SessionLocal, engine, get_db
 from utils import pydanticModels
 from utils.db import models
 from starlette.datastructures import UploadFile as StarletteUploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 
 # keep the SpooledTemporaryFile in-memory
 StarletteUploadFile.spool_max_size = 0
@@ -11,6 +15,20 @@ models.Base.metadata.create_all(bind = engine)
 
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(StockData.router)
 
 @app.get("/")
